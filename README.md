a28534a986bcd7b2a68ffd34f1a5e9bd4830bcb1
Erst086 <eresto08@gmail.com>
2024-11-26 07:41:44 -0600

    se cambia app

--- README.md
+++
@@ -1,3 +0,0 @@
-# Carrito
-Fvor de correr el archivo de base.sql primero y luego insert.sql
-Repo de PW2_Nodejs y BD
--- web/controllers/adminOps/adminController.js
+++
@@ -1,9 +0,0 @@
-//renderiza el formulario de inicio de sesion
-const inicio = (req, res) => {
-    console.log(req.session);
-    res.render("admin/inicioAdministrador", {
-        csrf: req.csrfToken(),
-    });
-};
-
-export {inicio};
